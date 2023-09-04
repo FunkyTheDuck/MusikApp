7caf3b5972bfccbd6bdde9014676dc51d19614cc
Simon Dal Kotus Jensen <dkjsimon@gmail.com>
2023-09-04 11:24:26 +0200

    Har fået lavet 2 methoder til at like og skip, som kalder til repository men ikke videre

---
+++ AppRepository/StartPageRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRepository
+{
+    public class StartPageRepository
+    {
+        public async Task<bool> LikeSongAsync(int userId, int songId)
+        {
+            return true;
+        }
+        public async Task<bool> SkipSongAsync(int userId, int songId)
+        {
+            return true;
+        }
+    }
+}
