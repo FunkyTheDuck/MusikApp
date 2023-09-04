a93aae7a4bf16fee645b30c014c20bb2f20715b1
Nicolai <nico622f@herningsholm.dk>
2023-09-04 15:37:02 +0200

    er færdig med ui til loign side mangler kun at koble createuser til og få backend op og køre med en eksisterene bruger

---
+++ MusikApp/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MusikApp.ViewModels
+{
+    public class LoginPageViewModel : BaseViewModels
+    {
+        public ICommand SpotifyUserCommand { get; set; }
+        public ICommand AppleUserCommand { get; set; }
+        public ICommand SoundCloudUserCommand { get; set; }
+
+
+        public LoginPageViewModel()
+        {
+            SpotifyUserCommand = new Command(SpotifyTapped);
+            AppleUserCommand = new Command(AppleTapped);
+            SoundCloudUserCommand = new Command(SoundCloudTapped);
+        }
+
+        public async void SpotifyTapped()
+        {
+            await (Application.Current.MainPage).DisplayAlert("Information", "Sign in with Spotify is comming soon", "OK");
+        }
+
+        public async void AppleTapped()
+        {
+            await (Application.Current.MainPage).DisplayAlert("Warning", "Sign in with Apple is comming soon", "OK");
+        }
+
+        public async void SoundCloudTapped()
+        {
+            await (Application.Current.MainPage).DisplayAlert("Warning", "Sign in with Sound Cloud is comming soon", "OK");
+        }
+    }
+}
