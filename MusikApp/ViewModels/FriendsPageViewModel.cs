74fe71cb056ef9152c27e44d3718e93f6e21dc6a
marc7781 <trold2003@gmail.com>
2023-09-14 11:11:31 +0200

    Revert "Revert "lave UI til friends og lave LastOnline til user model""
    This reverts commit eb377a7e73b12fe24deed1f9d7218c5fcf52466f.

--- MusikApp/ViewModels/FriendsPageViewModel.cs
+++ MusikApp/ViewModels/FriendsPageViewModel.cs
@@ -4,15 +4,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MusikApp.ViewModels
 {
     public class FriendsPageViewModel : BaseViewModels
     {
-        public List<User> friends { get; set; }
-        public FriendsPageViewModel() 
+        public List<User> Friends { get; set; }
+        public FriendsPageViewModel()
         {
-
+            Friends = new List<User>();
+            User user = new User()
+            {
+                Id = 1,
+                ProfilPicture = "profilbillede.jpg",
+                Name = "Justin",
+                LastName = "Bieber",
+                UserName = "JustinB",
+                Password = "Bruger123",
+                Mail = "justinbieber@hotmail.com",
+                IsPremium = true,
+                IsArtist = true,
+                LastOnline = DateTime.Now
+            };
+            User user1 = new User()
+            {
+                Id = 2,
+                ProfilPicture = "profilbillede.jpg",
+                Name = "Justin",
+                LastName = "Bieber",
+                UserName = "JustinB",
+                Password = "Bruger123",
+                Mail = "justinbieber@hotmail.com",
+                IsPremium = true,
+                IsArtist = true,
+                LastOnline = DateTime.Now
+            };
+            User user2 = new User()
+            {
+                Id = 3,
+                ProfilPicture = "profilbillede.jpg",
+                Name = "Justin",
+                LastName = "Bieber",
+                UserName = "JustinB",
+                Password = "Bruger123",
+                Mail = "justinbieber@hotmail.com",
+                IsPremium = true,
+                IsArtist = true,
+                LastOnline = DateTime.Now
+            };
+            Friends.Add(user);
+            Friends.Add(user1);
+            Friends.Add(user2);
+            OnPropChanged(nameof(Friends));
         }
 
     }
